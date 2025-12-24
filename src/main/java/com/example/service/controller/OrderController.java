cee77fce2cf835a772e7a7d343e8786dac2745db
Maheshbabu78 <77149821+Maheshbabu78@users.noreply.github.com>
2025-12-24 19:32:31 +0530

    myfirstcommit

---
+++ src/main/java/com/example/service/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.example.service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @GetMapping("/orders/health")
+    public String health() {
+        return "Order Service is UP";
+    }
+
+    @GetMapping("/orders")
+    public String getOrders() {
+        return "List of orders";
+    }
+}
